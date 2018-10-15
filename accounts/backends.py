dd6619e361c2b93a97711c3fc28407089f513c1e
Liam-Cafearo <lcafearo@gmail.com>
2018-10-15 07:34:14 +0100

    added code to use email field instead of normal username

---
+++ accounts/backends.py
@@ -0,0 +1,22 @@
+from models import User
+
+class EmailAuth(object):
+
+    def authenticate(self, email=None, password=None):
+        # grab user via supplied email and password
+        try:
+            user = User.objects.get(email=email)
+            if user.check_password(password):
+                return user
+        except User.DoesNotExist:
+            return None
+        
+    def get_user(self, user_id):
+        # Django auth system uses this to get an instance of the user
+        try:
+            user = user.objects.get(pk=user_id)
+            if user.is_active:
+                return user
+            return None
+        except User.DoesNotExist:
+            return None
