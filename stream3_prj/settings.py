557962fdd988b895e327973d2610012c02a800d0
Liam-Cafearo <lcafearo@gmail.com>
2018-10-15 07:33:29 +0100

    added AUTHENTICATION_BACKENDS so django uses this backend instead

--- stream3_prj/settings.py
+++ stream3_prj/settings.py
@@ -83,6 +83,11 @@ DATABASES = {
 
 AUTH_USER_MODEL = 'accounts.User'
 
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'accounts.backends.EmailAuth'
+)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
