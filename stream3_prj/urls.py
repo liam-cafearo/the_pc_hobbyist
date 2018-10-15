41c8c46fce4564261996e593338d621b4a27e1ee
Liam-Cafearo <lcafearo@gmail.com>
2018-10-15 12:38:22 +0100

    added logout url and corrected login url

--- stream3_prj/urls.py
+++ stream3_prj/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     # Accounts views
     url(r'^register/$', accounts_views.register, name='register'),
     url(r'^profile/$', accounts_views.profile, name='profile'),
-    url(r'login/$', accounts_views.login, name='login'),
+    url(r'^login/$', accounts_views.login, name='login'),
+    url(r'^logout/$', accounts_views.logout, name='logout'),
 ]
