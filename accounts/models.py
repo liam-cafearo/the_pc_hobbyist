98e315e3373ac8f65620ad352e6bc7bd8fc01e98
Liam-Cafearo <lcafearo@gmail.com>
2018-10-15 08:30:46 +0100

    corrected formatting

--- accounts/models.py
+++ accounts/models.py
@@ -13,16 +13,17 @@ class AccountUserManager(UserManager):
         now = timezone.now()
         if not email:
             raise ValueError('The given username must be set')
-        
+
         email = self.normalize_email(email)
         user = self.model(username=email, email=email,
-                        is_staff=is_staff, is_active=True,
-                        is_superuser=is_superuser,
-                        date_joined=now, **extra_fields)
+                          is_staff=is_staff, is_active=True,
+                          is_superuser=is_superuser,
+                          date_joined=now, **extra_fields)
         user.set_password(password)
         user.save(using=self.db)
 
         return user
+
 
 class User(AbstractUser):
     objects = AccountUserManager()
