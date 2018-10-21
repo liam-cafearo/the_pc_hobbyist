4ae97781a54394f46d408f9cdb2f98302b3a6686
Liam-Cafearo <lcafearo@gmail.com>
2018-10-21 22:43:11 +0100

    corrected js to prevent submission

--- static/js/contact.js
+++ static/js/contact.js
@@ -1,11 +1,15 @@
-$(document).ready(function() {
-    $('#contactSubmit').click(function () {
-        var name = document.contactForm.name.value;
-        var email = document.contactForm.email.value;
-        var message = document.contactForm.message.value;
-        if(name || email || message === "") {
+$(document).ready(function () {
+
+    $('#contactSubmit').click(function (event) {
+        if (document.contactForm.name.value === "") {
             $('#emptyField').modal("show");
-            return false;
+            event.preventDefault()
+        } else if (document.contactForm.email.value === "") {
+            $('#emptyField').modal("show");
+            event.preventDefault()
+        } else if (document.contactForm.message.value === "") {
+            $('#emptyField').modal("show");
+            event.preventDefault()
         }
     });
 });
