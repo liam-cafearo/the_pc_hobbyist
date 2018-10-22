e6fa22cbb3d10dc97d5b74635cabfdd3f5ed941d
Liam-Cafearo <lcafearo@gmail.com>
2018-10-22 19:57:35 +0100

    added EMAIL_HOST_USER to env var

--- settings/base.py
+++ settings/base.py
@@ -155,7 +155,7 @@ TINYMCE_JS_ROOT = os.path.join(BASE_DIR, "static", 'js', 'tinymce', 'tinymce.min
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'thepchobbyist2018@gmail.com'
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
 EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 
 ADMINS = (
