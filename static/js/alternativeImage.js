6b916e935d34fa45c26788dd1cefbc7586282450
Liam-Cafearo <lcafearo@gmail.com>
2018-10-24 17:04:21 +0100

    corrected img.src

--- static/js/alternativeImage.js
+++ static/js/alternativeImage.js
@@ -1,6 +1,6 @@
 //  Inspired by Stack Overflow post and expanded upon, listed in credits.
 function alternativeImage(image) {
     image.onerror = "";
-    image.src = "https://placehold.it/100";
+    image.src = "https://thepchobbyist.s3.amazonaws.com/static/img/default_avatar.jpg";
     return true;
 }
