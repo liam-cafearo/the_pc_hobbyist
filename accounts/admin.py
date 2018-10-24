48a9eb2f770ec24347a0af061cb9a54e0ddd9cb4
Liam-Cafearo <lcafearo@gmail.com>
2018-10-24 17:03:55 +0100

    registered users so that users can be administered from admin page

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
 # Register your models here.
+# Added for easier user adminstration
+admin.site.register(User, UserAdmin)
